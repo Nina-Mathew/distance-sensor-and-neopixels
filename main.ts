0cc31f02ca12b5971a3cff26c848e99d870f0f96
Nina-Mathew <71400436+Nina-Mathew@users.noreply.github.com>
2020-10-02 12:25:39 -0400

    arranged some blocks to different areas

--- main.ts
+++ main.ts
@@ -2,24 +2,27 @@
 // Created on: 10/2/20
 // This program combines Neopixels and distance sensor
 let distancetotheobject = 0
-basic.clearScreen()
+let distance = 0
+let strip2: neopixel.Strip = null
 basic.showIcon(IconNames.Yes)
+basic.clearScreen()
+strip2.clear()
 basic.pause(500)
-let strip2 = neopixel.create(DigitalPin.P16, 2, NeoPixelMode.RGB)
-basic.pause(1000)
-while (distancetotheobject >= 4) {
-    for (let distancetotheobject2 = 0; distancetotheobject2 <= 15; distancetotheobject2++) {
-        basic.showNumber(distancetotheobject2)
-        strip2.setPixelColor(1, neopixel.colors(NeoPixelColors.Green))
-        strip2.show()
-    }
+strip2 = neopixel.create(DigitalPin.P16, 2, NeoPixelMode.RGB)
+while (distance >= 4) {
+    basic.showNumber(distance)
+    strip2.setPixelColor(1, neopixel.colors(NeoPixelColors.Green))
+    strip2.show()
 }
-while (distancetotheobject <= 4) {
-    for (let distancetotheobject2 = 0; distancetotheobject2 <= 15; distancetotheobject2++) {
-        basic.showNumber(distancetotheobject2)
-        strip2.setPixelColor(0, neopixel.colors(NeoPixelColors.Blue))
-        strip2.show()
-    }
+while (distance <= 4) {
+    basic.showNumber(distance)
+    strip2.setPixelColor(0, neopixel.colors(NeoPixelColors.Blue))
+    strip2.show()
+}
+while (distance <= 1) {
+    basic.showNumber(distance)
+    strip2.setPixelColor(2, neopixel.colors(NeoPixelColors.Red))
+    strip2.show()
 }
 basic.forever(function () {
     distancetotheobject = sonar.ping(
