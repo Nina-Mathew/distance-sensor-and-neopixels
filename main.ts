8df209f5b432471204139e09f1c3143449f7fc91
Nina-Mathew <71400436+Nina-Mathew@users.noreply.github.com>
2020-10-02 12:00:56 -0400

    loop added

--- main.ts
+++ main.ts
@@ -7,20 +7,24 @@ basic.showIcon(IconNames.Yes)
 basic.pause(500)
 let strip2 = neopixel.create(DigitalPin.P16, 2, NeoPixelMode.RGB)
 basic.pause(1000)
+while (distancetotheobject >= 4) {
+    for (let distancetotheobject2 = 0; distancetotheobject2 <= 15; distancetotheobject2++) {
+        basic.showNumber(distancetotheobject2)
+        strip2.setPixelColor(1, neopixel.colors(NeoPixelColors.Green))
+        strip2.show()
+    }
+}
+while (distancetotheobject <= 4) {
+    for (let distancetotheobject2 = 0; distancetotheobject2 <= 15; distancetotheobject2++) {
+        basic.showNumber(distancetotheobject2)
+        strip2.setPixelColor(0, neopixel.colors(NeoPixelColors.Blue))
+        strip2.show()
+    }
+}
 basic.forever(function () {
     distancetotheobject = sonar.ping(
     DigitalPin.P1,
     DigitalPin.P2,
     PingUnit.Centimeters
     )
-    basic.showNumber(distancetotheobject)
-    if (distancetotheobject < 4) {
-        strip2.clear()
-        strip2.setPixelColor(3, neopixel.colors(NeoPixelColors.Blue))
-        strip2.show()
-    } else if (distancetotheobject > 4) {
-        strip2.clear()
-        strip2.setPixelColor(1, neopixel.colors(NeoPixelColors.Green))
-        strip2.show()
-    }
 })
