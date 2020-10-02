d67dc98a9a0c80923d8617ab12f6cb5437fc4b09
Nina-Mathew <71400436+Nina-Mathew@users.noreply.github.com>
2020-10-02 10:57:30 -0400

    blue and green light added

--- main.ts
+++ main.ts
@@ -1,3 +1,24 @@
+// Created by: Nina Mathew
+// Created on: 10/2/20
+// This program combines Neopixels and distance sensor
+let distancetotheobject = 0
+basic.clearScreen()
+basic.showIcon(IconNames.Yes)
+basic.pause(500)
+let strip2 = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
+basic.pause(1000)
+strip2.clear()
+strip2.setPixelColor(3, neopixel.colors(NeoPixelColors.Blue))
+strip2.show()
+basic.pause(1000)
+strip2.clear()
+strip2.setPixelColor(1, neopixel.colors(NeoPixelColors.Green))
+strip2.show()
 basic.forever(function () {
-	
+    distancetotheobject = sonar.ping(
+    DigitalPin.P1,
+    DigitalPin.P2,
+    PingUnit.Centimeters
+    )
+    basic.showNumber(distancetotheobject)
 })
