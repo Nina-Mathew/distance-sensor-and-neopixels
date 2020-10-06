8342daa4c55a66c52226293322ab72349239d0e8
Nina-Mathew <71400436+Nina-Mathew@users.noreply.github.com>
2020-10-05 21:26:02 -0400

    added blue neopixels with cm 4,3,2, and 1

--- main.ts
+++ main.ts
@@ -1,33 +1,47 @@
 // Created by: Nina Mathew
 // Created on: 10/2/20
 // This program combines Neopixels and distance sensor
-let distancetotheobject = 0
-let distance = 0
 let strip2: neopixel.Strip = null
+let distancetotheobject = 0
 basic.showIcon(IconNames.Yes)
 basic.clearScreen()
-strip2.clear()
 basic.pause(500)
-strip2 = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
-while (distance > 4) {
-    basic.showNumber(distance)
-    strip2.showColor(neopixel.colors(NeoPixelColors.Green))
-    strip2.show()
-}
-while (distance <= 4) {
-    basic.showNumber(distance)
-    strip2.showColor(neopixel.colors(NeoPixelColors.Blue))
-    strip2.show()
-}
-while (distance <= 1) {
-    basic.showNumber(distance)
-    strip2.showColor(neopixel.colors(NeoPixelColors.Red))
-    strip2.show()
-}
 basic.forever(function () {
     distancetotheobject = sonar.ping(
     DigitalPin.P1,
     DigitalPin.P2,
     PingUnit.Centimeters
     )
+})
+basic.forever(function () {
+    let distance = 0
+    strip2 = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
+    if (distance > 4) {
+        basic.showNumber(distance)
+        strip2.showColor(neopixel.colors(NeoPixelColors.Green))
+        strip2.show()
+    }
+    if (distance == 4) {
+        basic.showNumber(distance)
+        strip2.showColor(neopixel.colors(NeoPixelColors.Blue))
+        strip2.show()
+    }
+    if (distance == 3) {
+        basic.showNumber(distance)
+        strip2.setPixelColor(0, neopixel.colors(NeoPixelColors.Blue))
+        strip2.setPixelColor(1, neopixel.colors(NeoPixelColors.Blue))
+        strip2.setPixelColor(2, neopixel.colors(NeoPixelColors.Blue))
+        strip2.show()
+    }
+    if (distance == 2) {
+        basic.showNumber(distance)
+        strip2.setPixelColor(0, neopixel.colors(NeoPixelColors.Blue))
+        strip2.setPixelColor(1, neopixel.colors(NeoPixelColors.Blue))
+        strip2.show()
+    }
+    if (distance < 1) {
+        basic.showNumber(distance)
+        strip2.showColor(neopixel.colors(NeoPixelColors.Red))
+        strip2.show()
+    }
 })
